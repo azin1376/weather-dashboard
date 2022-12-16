17ddc6e3db8f7c4a7dd162c52f654610b40b54ec
azin1376 <118169244+azin1376@users.noreply.github.com>
2022-12-17 01:07:35 +0330

    added current button

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ function searchCity(city) {
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(weatherUrl).then(showTemp);
 }
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showLocation);
+}
+function showLocation(position) {
+  console.log(position);
+  let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
+  let long = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&untits=${units}`;
+  axios.get(apiUrl).then(showTemp);
+}
 function showTemp(response) {
   document.querySelector("h1").innerHTML = `${response.data.name}`;
   document.querySelector("#deg").innerHTML = `${Math.round(
@@ -33,7 +46,7 @@ function showTemp(response) {
   ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
   document.querySelector(
     "#pressure"
-  ).innerHTML = `Pressure: ${response.data.main.pressure}`;
+  ).innerHTML = `Main: ${response.data.weather[0].main}`;
 }
 let now = new Date();
 let liii = document.querySelector("#day");
@@ -41,6 +54,7 @@ let date = now.getDate();
 let hour = now.getHours();
 let min = now.getMinutes();
 let search = document.querySelector("#search");
+let current = document.querySelector("#current");
 let days = [
   "Sunday",
   "Monday",
@@ -60,4 +74,5 @@ if (hour < 10) {
 }
 liii.innerHTML = `${day} ${hour}:${min}`;
 search.addEventListener("submit", getCity);
+current.addEventListener("click", getLocation);
 searchCity("Ahvaz");
